6b04bfa4e31faa7bea24762b57d5bbb8256e635f
nativepapaya <akokorinime@gmail.com>
2020-12-11 17:16:44 -0500

    fixed more stuff.

--- .config/fish/functions/vpm.fish
+++ .config/fish/functions/vpm.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function vpm --wraps='vpm --color' --wraps='vpm --color=true' --wraps='sudo vpm --color' --wraps='sudo vpm --color=true' --wraps='sudo vpm --color=yes' --wraps='doas vpm --color=yes' --description 'alias vpm=doas vpm --color=yes'
-  doas vpm --color=yes $argv;
+function vpm --wraps='sudo vpm --color=yes' --description 'alias vpm=sudo vpm --color=yes'
+  sudo vpm --color=yes $argv;
 end
