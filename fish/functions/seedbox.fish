89a271aea4177ca56a619172b764cfa059e4817f
nativepapaya <akokorinime@gmail.com>
2020-12-11 16:52:59 -0500

    removed seedbox

--- fish/functions/seedbox.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function seedbox --wraps='lftp sftp://arviel:@81.171.22.169' --description 'alias seedbox=lftp sftp://arviel:@81.171.22.169'
-  lftp sftp://arviel:@81.171.22.169 $argv;
-end
